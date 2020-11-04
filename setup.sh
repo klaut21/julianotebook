d1e8e4b4baa41e75c345f2f31e3cf8bc40857c8b
Michael Kuegeler (Cloud) <mkuegeler@googlemail.com>
2020-11-04 11:58:03 +0100

    .gitignore updated

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # Generate a password
 jupyter notebook password
 # Wrote hashed password to /Users/m.kuegeler/.jupyter/jupyter_notebook_config.json
+# Copy hash into .env
 
 # Generate cert:
 openssl req -x509 -nodes -newkey rsa:2048 -keyout jupyter.pem -out ./ssl-certs/jupyter.pem
